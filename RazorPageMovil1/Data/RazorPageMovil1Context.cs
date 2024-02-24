6953d207a00528fa758684d318f2493c4a58d456
GuillermoStrong <luisguillermofuerte@outlook.com>
2024-02-24 13:20:00 -0600

    Add project files.

---
+++ RazorPageMovil1/Data/RazorPageMovil1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPageMovil1.Models;
+
+namespace RazorPageMovil1.Data
+{
+    public class RazorPageMovil1Context : DbContext
+    {
+        public RazorPageMovil1Context (DbContextOptions<RazorPageMovil1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPageMovil1.Models.Movie> Movie { get; set; } = default!;
+    }
+}
